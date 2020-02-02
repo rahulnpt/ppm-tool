d6188feed001bbea445832a07ba7d385c6409db0
Rahul <Rahul.npt@gmail.com>
2020-02-02 19:15:08 +0530

    first commit

---
+++ ppmtool/src/test/java/com/mhuffers/PpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhuffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
